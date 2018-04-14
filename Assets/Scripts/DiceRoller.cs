ce1b4ee801cba9e884944e23bcaffb474c50be22
Micah Schuster <micahdschuster@gmail.com>
2018-04-14 18:55:26 -0400

    Added Dice images
    When rolled, the dice display an appropriate image

--- Assets/Scripts/DiceRoller.cs
+++ Assets/Scripts/DiceRoller.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiceRoller : MonoBehaviour {
 
 	public int[] diceValues = new int[4];
 	public int diceTotal;
+	public Sprite[] DiceImageOne;
+	public Sprite[] DiceImageZero;
 
 	// Use this for initialization
 	void Start () {
@@ -26,7 +29,21 @@ public class DiceRoller : MonoBehaviour {
 		for (int i = 0; i < diceValues.Length; i++) {
 			diceValues [i] = Random.Range (0, 2); //inclusive min, exclusive max
 			diceTotal += diceValues[i];
+
+
+			//Update the visuals to show dice roll
+			//This could involve playing an annimation (2D or 3D)
+
+			//We have four childre, each is a die image
+			//grab that child and update it's image component to use a
+			//random sprite of the correct type
+			if(diceValues[i]==0){
+				this.transform.GetChild (i).GetComponent<Image> ().sprite = DiceImageZero[Random.Range (0, DiceImageZero.Length)];
+			} else{
+				this.transform.GetChild (i).GetComponent<Image> ().sprite = DiceImageOne[Random.Range (0, DiceImageOne.Length)];
+			}
 		}
+
 
 		Debug.Log ("Rolled: " + diceValues[0]+" "+ diceValues[1]+" "+ diceValues[2]+" "+ diceValues[3]+" " + " (" + diceTotal + ") ");
 	}
