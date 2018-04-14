e8427e00368e7c6a1366696ba3a96219d1e30526
Micah Schuster <micahdschuster@gmail.com>
2018-04-13 20:32:50 -0400

    Added Dice Rolling
    UI +RNG rolling

---
+++ Assets/Scripts/DiceRoller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoller : MonoBehaviour {
+
+	public int[] diceValues = new int[4];
+	public int diceTotal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RollTheDice(){
+		// In Ur, you roll four dice (50-50 chance)
+		// half the faces are 0, half are 1
+		// Not physics enabled dice, just RNG for now
+
+		diceTotal = 0;
+		for (int i = 0; i < diceValues.Length; i++) {
+			diceValues [i] = Random.Range (0, 2); //inclusive min, exclusive max
+			diceTotal += diceValues[i];
+		}
+
+		Debug.Log ("Rolled: " + diceValues[0]+" "+ diceValues[1]+" "+ diceValues[2]+" "+ diceValues[3]+" " + " (" + diceTotal + ") ");
+	}
+}
