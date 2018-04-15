ee7194cb3572ffd365e5f71510638b9021f68f63
Micah Schuster <micahdschuster@gmail.com>
2018-04-14 20:06:01 -0400

    Added Stone movement on board

--- Assets/Scripts/PlayerStone.cs
+++ Assets/Scripts/PlayerStone.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerStone : MonoBehaviour {
 
 	DiceRoller theDiceRoller;
+	public Tile startingTile;
+	Tile currentTile=null;
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +20,7 @@ public class PlayerStone : MonoBehaviour {
 
 	void OnMouseUp(){
 		//TODO: is the mouse over a UI element? Ignore click if so
-		Debug.Log ("Click");
+		//Debug.Log ("Click");
 
 		//TODO is it our turn
 
@@ -27,5 +29,42 @@ public class PlayerStone : MonoBehaviour {
 			//you can't move!
 			return;
 		}
+		int spacesToMove = theDiceRoller.diceTotal;
+
+		//where should we end up?
+		if(spacesToMove==0){
+			return;
+		}
+
+		Tile finalTile = null;
+		for (int i = 0; i < spacesToMove; i++) {
+			if (currentTile == null) { //on starting space
+				finalTile = startingTile;
+				currentTile = finalTile;
+			} else {
+				if (currentTile.nextTiles == null || currentTile.nextTiles.Length == 0) {
+					//We have reaced the end and should score
+					Debug.Log ("Score!");
+					Destroy (gameObject);
+					return;
+				} else if (currentTile.nextTiles.Length > 1) {
+					//branch based to player ID
+					finalTile = currentTile.nextTiles [0];
+					currentTile = finalTile;
+				} else {
+					finalTile = currentTile.nextTiles [0];
+					currentTile = finalTile;
+				}
+			}
+		}
+		if(finalTile==null){
+			//Somthing werid happens
+			return;
+		}
+
+		//no move to final tile
+		//TODO: Animate
+		this.transform.position=finalTile.transform.position;
+
 	}
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	public Tile[] nextTiles; //use array to handle branching
+							 //see inspector for the "linked list"
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
